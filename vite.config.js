48b6caac34490cb31a23ed918d1fc58a4a8ba32e
amadeusj5 <amadeusjosue5@gmail.com>
2024-04-03 04:01:58 -0600

    build: :rocket: adding deploy configuration

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import { resolve } from 'path'
 import { defineConfig } from 'vite'
 
 export default defineConfig({
+  base: '/asmr-landing-pages/',
   build: {
     rollupOptions: {
       input: {
